4d7a66623e10f1afa321650fea8e3e56896dfb2e
1994cjw@gmail.com <1994cjw@gmail.com>
2022-06-09 14:04:45 +0900

    문자열결합 미완성

---
+++ j/Algo1.java
@@ -0,0 +1,19 @@
+package com.survivalcoding;
+
+import java.util.Scanner;
+
+public class Algo {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int number = in.nextInt();
+		String[] strs = new String[number];
+		for (int i = 0; i < number; i++)
+			strs[i] = in.next();
+
+		System.out.print("Hello ");
+		
+		for (int i = 0; i < number; i++)
+			
+			System.out.print(strs[i]+ ".");
+	}
+}
